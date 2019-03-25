50f17b246297f6027a04365c3417af1f149300bf
Arturo Amador Paulino <amadorassassin@gmail.com>
2019-03-25 09:56:33 -0600

    cambios simulador pestaña ventana para código

---
+++ js/simulador.js
@@ -0,0 +1,9 @@
+// Projama ejecutado en caída libre
+
+//constantes conocidad 
+const g = 9.81;
+const airDensity = 1.29;
+
+let a = g; //la aceleración de la grvedad
+let t = 72; //tiempo de vuelo
+let v = -g * t;
